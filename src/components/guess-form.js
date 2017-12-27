679b6a9673d311f373a8cfd4a34ed2163202e1d7
Kelsey Butler <kelseybutler@Kelseys-MBP.attlocal.net>
2017-12-27 10:15:20 -0500

    updates for functional app

--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import GuessButton from './guess-button';
-import GuessList from './guess-list';
 
-export default function GuessForm(props) {
+export default class GuessForm extends React.Component {
   onSubmit(e) {
     e.preventDefault();
+
+    if(this.props.onMakeGuess) {
+      const value = this.input.value;
+      this.props.onMakeGuess(value);
+    }
     this.input.value = '';
     this.input.focus();
   }
 
   render() {
     return (
-      <div className="result">
-      </div>
-      <form onSubmit={e => e.preventDefault()}>
+      <form onSubmit={event => this.onSubmit(event)}>
         <label htmlFor="guess">Guess</label>&emsp;
           <input type="number" id="guess" name="guess" min="1" max="100" ref={input => (this.input=input)} required placholder="Enter your Guess"/>
         <GuessButton id="guessButton"/>
       </form>
     );
+  }
 }
